39128986768ab2c94354d3a791d8c15cbd931682
shubh <39399239+beshubh@users.noreply.github.com>
2026-08-07 17:37:52 +0530

    Update solution for 0621 Task Scheduler

--- arrays/0621-task-scheduler/solution.py
+++ arrays/0621-task-scheduler/solution.py
@@ -4,23 +4,17 @@ import collections
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        pq = []
-        task_counts = list(sorted(list(collections.Counter(tasks).values())))
-        q = collections.deque(task_counts)
-        cycles = 0 # 6
+        task_counts = collections.Counter(tasks).values()
+        pq = [-t for t in task_counts]
+        heapq.heapify(pq)
+        q = collections.deque()
+        cycles = 0
         while q or pq:
             cycles += 1
-            if q:
-                task_count = q.pop() # 2
+            if pq:
+                task_count = -heapq.heappop(pq)
                 if task_count - 1 > 0: # 1 > 0 yes
-                    heapq.heappush(pq, (cycles + n, -(task_count - 1)))
-            if pq and pq[0][0] <= cycles: # 4 <= 4 yes
-                tsk = -heapq.heappop(pq)[1]
-                if q and q[-1] <= tsk:
-                    q.append(tsk)
-                elif q:
-                    q.appendleft(tsk)
-                else:
-                    q.append(tsk)
-
+                    q.append((cycles + n, task_count - 1))
+            if q and q[0][0] <= cycles:
+                heapq.heappush(pq, -q.popleft()[1])
         return cycles

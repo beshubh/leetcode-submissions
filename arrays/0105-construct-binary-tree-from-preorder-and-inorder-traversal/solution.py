82fa76f54a3abc4c2daecddda2b7cf73b10cc89b
shubh <39399239+beshubh@users.noreply.github.com>
2026-08-19 09:34:40 +0530

    Update solution for 0105 Construct Binary Tree from Preorder and Inorder Traversal

--- arrays/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
+++ arrays/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
@@ -6,19 +6,17 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-
-        def build(preorder: list[int], inorder: list[int]):
-            if not inorder or not preorder:
+        
+        def go(po, io):
+            if not po or not io:
                 return None
-            root = TreeNode(preorder[0])
-            root_idx = inorder.index(preorder[0])
-            left_io = inorder[:root_idx]
-            if left_io:
-                root.left = build(preorder[1:1 + len(left_io)], left_io)
-                right_io = inorder[root_idx + 1:]
-                root.right = build(preorder[1 + len(left_io):], right_io)
-            else:
-                root.left = None
-                root.right = build(preorder[1:], inorder[root_idx + 1:])
+            root = TreeNode(po[0]) 
+            idx = io.index(po[0])
+            left_io = io[:idx]
+            right_io = io[idx + 1:]
+            left_po = po[1:len(left_io) + 1]
+            right_po = po[len(left_io) + 1:]
+            root.left = go(left_po, left_io)
+            root.right = go(right_po, right_io)
             return root
-        return build(preorder, inorder)
+        return go(preorder, inorder) 

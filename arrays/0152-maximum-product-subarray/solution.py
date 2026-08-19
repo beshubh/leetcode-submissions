da0dd3a247ad11ec6e4d2c8546d15e46cfb15c6f
shubh <39399239+beshubh@users.noreply.github.com>
2026-08-19 10:20:02 +0530

    Update solution for 0152 Maximum Product Subarray

--- arrays/0152-maximum-product-subarray/solution.py
+++ arrays/0152-maximum-product-subarray/solution.py
@@ -1,17 +1,16 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        max_ending_here = nums[0]
-        min_ending_here = nums[0]
         global_max = nums[0]
+        current_max = nums[0]
+        current_min = nums[0]
         for i in range(1, len(nums)):
             global_max = max(
                 global_max,
-                max_ending_here * nums[i],
-                min_ending_here * nums[i],
+                current_max*nums[i],
+                current_min*nums[i],
                 nums[i]
             )
-            old_max = max_ending_here
-            max_ending_here = max(nums[i], max_ending_here * nums[i], min_ending_here * nums[i])
-            min_ending_here = min(nums[i],  min_ending_here * nums[i], old_max * nums[i])
-        return max(global_max, max_ending_here)
-
+            temp = current_max
+            current_max = max(current_max * nums[i], nums[i], current_min * nums[i])
+            current_min = min(current_min * nums[i], nums[i], temp * nums[i])
+        return max(global_max, current_max, current_min)

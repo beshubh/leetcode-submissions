09e3a69d47737d69f74c239e49effab8d03c339c
shubh <39399239+beshubh@users.noreply.github.com>
2026-08-19 10:31:04 +0530

    Update solution for 1352 Maximum Profit in Job Scheduling

--- arrays/1352-maximum-profit-in-job-scheduling/solution.py
+++ arrays/1352-maximum-profit-in-job-scheduling/solution.py
@@ -2,10 +2,13 @@ class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         data = list(zip(startTime, endTime, profit))
         data.sort(key=lambda x:x[0])
-        n = len(data) 
-        dp = [0] * (n + 1)
-        for i in range(n - 1, -1, -1):
-            l, r = i + 1, n - 1
+        cache = {}
+        def go(i: int):
+            if i in cache:
+                return cache[i]
+            if i >= len(profit):
+                return 0
+            l, r = i + 1, len(profit) - 1
             while l <= r:
                 m = (l + r) // 2
                 if data[m][0] < data[i][1]:
@@ -13,8 +16,8 @@ class Solution:
                 else:
                     r = m - 1
             j = r + 1
-            take = data[i][2] + dp[j]
-            skip = dp[i + 1]
-            dp[i] = max(take, skip)
-        return dp[0]
-
+            take = data[i][2] + go(j)
+            skip = go(i + 1)
+            cache[i] = max(take, skip)
+            return cache[i]
+        return go(0)
